f3d57b3bf603b938ad0972dfe4d1a262892bf61f
Tony de Faria <tony.faria@icloud.com>
2021-06-27 17:02:15 +0200

    netlify config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  reactStrictMode: true,
-}
+// next.config.js
+
+module.exports = withImages({
+  webpack5: true,
+  i18n: {
+    locales: ["de", "en"],
+    defaultLocale: "de",
+    localeDetection: false
+  },
+  target: "serverless",
+  reactStrictMode: true
+})

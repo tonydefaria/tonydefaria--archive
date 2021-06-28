b98c21a3d3f21976480553ad64d097299120b9fd
Tony de Faria <tony.faria@icloud.com>
2021-06-28 10:25:01 +0200

    clean up

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles/globals.css"
+// App
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
+// Index
+
 import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Tony de Faria</title>
         <meta name="description" content="I'm a self-taught creative thinker and, I like to use words, images and code to express my thoughts and ideas. I'm passionate about stories that have humanity, compassion and food." />

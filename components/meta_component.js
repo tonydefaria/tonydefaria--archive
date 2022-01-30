c54a0e98c43de2fed0938b1ca1662588335f47a0
Tony de Faria <me@tonydefaria.com>
2022-01-30 19:08:49 +0100

    added author

--- components/meta_component.js
+++ components/meta_component.js
@@ -15,10 +15,9 @@ export default function MetaComponent({hankyoProject, meta}) {
     baseURL = meta.url
   }
   const title = hankyoProject.project.title + " | " + meta.title
+  const author = hankyoProject.project.global_attributes.find(({uid}) => uid === "vVJh6fY3hXPpTi1mnhu2g6wG").value
   const twitter_handle = hankyoProject.project.global_attributes.find(({uid}) => uid === "dZabogRgkov9zRRh7oj47r8q").value
   const twitter_card = hankyoProject.project.global_attributes.find(({uid}) => uid === "r5BSc98jYfUZbNLxZPVSesHP").value
-
-  console.log(twitter_handle)
 
   return (
     <Head>
@@ -30,6 +29,9 @@ export default function MetaComponent({hankyoProject, meta}) {
       <meta name="description"          content={meta.description} />
       <meta name="keywords"             content={meta.keywords} />
       <link rel="canonical"             href={baseURL} />
+
+      {/* MISC */}
+      <meta name="author"               content={author} />
 
       {/* Open Graph */}
       <meta property="og:type"          content={hankyoProject.project.type_of} />

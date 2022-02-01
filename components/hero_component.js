bebddff67ebba097caa10eb677e375ed8d136627
Tony de Faria <me@tonydefaria.com>
2022-02-01 22:06:50 +0100

    update packages

--- components/hero_component.js
+++ components/hero_component.js
@@ -4,16 +4,15 @@
 
 export default function HeroComponent({hankyoSection}) {
 
-  const title = hankyoSection.section.blocks.find(({uid}) => uid === "RaMPALYXTApsxvufdTRdRkYT").title
-  // const description = hankyoSection.section.blocks.find(({uid}) => uid === "VCsvw54dXS3kwqcFVaSB8zcr").description
+  const hero = hankyoSection.section.blocks.find(({uid}) => uid === "wqq2dxdWkWsqRwjWAbiCEpbx")
 
   return (
     <div className="hero">
       <div className="hero-box">
         <div className="hero-row">
-          <h1 className="font-size-display"><span className="black-bg white-cl padded">{title}</span></h1>
+          <h1 className="font-size-display"><span className="black-bg white-cl padded">{hero.title}</span></h1>
           <hr className="separator-s" />
-          <p className="font-size-xxl"><span className="black-cl white-bg padded">Photography allows me to relate with people around the planet and see the world from different perspectives.</span></p>
+          <p className="font-size-xxl"><span className="black-cl white-bg padded">{hero.description}</span></p>
         </div>
       </div>
     </div>
--- components/social_networks_component.js
+++ components/social_networks_component.js
@@ -7,10 +7,10 @@ import Image from "next/image"
 
 export default function SocialNetworksComponent({project, social_networks}) {
   return (
-    <div className="networks flex-h-center">
-      <ul className="networks-box">
+    <div className="social-networks flex-h-center">
+      <ul className="social-networks-box">
         {social_networks.map((social_network) => (
-          <li key={social_network.uid} className="networks-item">
+          <li key={social_network.uid} className="social-networks-item">
             <a className="link" rel="noreferrer noopener" href={social_network.url} target="_blank">
               <div className="icon">
                 <Image src={social_network.image} width={64} height={64} alt="icon" title={project.title}/>

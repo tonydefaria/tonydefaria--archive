493439889580f2fd7f90b32bd24084f95616743f
Tony de Faria <me@tonydefaria.com>
2022-02-22 20:16:17 +0100

    update packages

--- components/scripts_component.js
+++ components/scripts_component.js
@@ -9,7 +9,7 @@ export default function ScriptsComponent() {
     <>
       {/* Analytics */}
       {/* onLoad={(e) => { console.error("Loaded", e)}} */}
-      <Script async="true" defer="true" src="https://cabin.tonydefaria.com/hello.js" strategy="beforeInteractive" />
+      <Script async defer src="https://cabin.tonydefaria.com/hello.js" strategy="beforeInteractive" />
     </>
   )
 }
